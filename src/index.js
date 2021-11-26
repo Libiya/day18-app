25e096c4ed0ae58828578eb922fe3f3900b224db
Mona Lisa <libiyapathrose99@gmail.com>
2021-11-26 13:35:40 +0530

    day18-app

---
+++ src/index.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import FavoriteColor from './components/usedemo';
+//import Car from './components/usedemostate';
+//import Counter from './components/useeffectdemo';
+//import Component1 from './components/democontext';
+import './index.css';
+//import Appuseref from './components/demouserref';
+//import Todos from './components/usereducer.js';
+//import Todo from './components/callbackcomp/Todo-index';
+//import UseMemoApp from './components/useMemoHookdemo';
+import ToolbarApp from './components/usecustomhook';
+//import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+/*ReactDOM.render(
+  <React.StrictMode>
+    <Todos />
+  </React.StrictMode>,
+  document.getElementById('root')
+);*/
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ToolbarApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+/*ReactDOM.render(
+  <React.StrictMode>
+    <Todo />
+  </React.StrictMode>,
+  document.getElementById('root')
+);*/
+
+
+/*ReactDOM.render(
+  <React.StrictMode>
+    <UseMemoApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);*/
+
+
+/*ReactDOM.render(
+  <React.StrictMode>
+    <Appuseref />
+  </React.StrictMode>,
+  document.getElementById('root')
+);*/
+
+
+/*ReactDOM.render(
+  <React.StrictMode>
+    <Car />
+  </React.StrictMode>,
+  document.getElementById('root')
+);*/
+
+/*ReactDOM.render(
+  <React.StrictMode>
+    <Counter />
+  </React.StrictMode>,
+  document.getElementById('root')
+);*/
+
+/*ReactDOM.render(
+  <React.StrictMode>
+    <Component1 />
+  </React.StrictMode>,
+  document.getElementById('root')
+);*/
+
+/*ReactDOM.render(
+  <React.StrictMode>
+    <FavoriteColor />
+  </React.StrictMode>,
+  document.getElementById('root')
+);*/
+
+
+/*ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);*/
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
